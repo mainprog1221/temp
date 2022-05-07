726a302ef26eaed6830793cb89a07b16e3575a74
zhufang1221 <zhufang1221@163.com>
2022-05-07 14:22:33 +0800

    master commit 3

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-    print("Hello git!")
-    print("Hello git2!")
+    print("Hello master git1!")
+    print("Hello master git2!")
     print("Hot fix!")
 
 
